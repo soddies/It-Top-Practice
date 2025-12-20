f10e64cfb0ee301ea21ee3db9ba9bd596b498b73
Ekaterina Pchelkina <ktrn.pchelkina@gmail.com>
2025-12-20 15:40:52 +0300

    Функция 6. Базовый визуал

--- functions/func6.js
+++ functions/func6.js
@@ -1,4 +1,15 @@
 // отчет по сданным ДЗ
+const { Markup } = require('telegraf');
+
 module.exports = async (ctx) => {
-    await ctx.reply('Функция 6 в разработке...')
-}
+    ctx.session.isInFunctionMenu = false;
+    
+    await ctx.reply('Вы выбрали: 6');
+    await ctx.reply("Пожалуйста, загрузите файл в формате XLS/XLSX", 
+        Markup.keyboard([
+            ['Вернуться в меню']
+        ])
+        .oneTime()
+        .resize()
+    );
+};
