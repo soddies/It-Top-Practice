dbf4c469c0de6d9f67ed582aeb49cf83677abafa
Ekaterina Pchelkina <ktrn.pchelkina@gmail.com>
2025-12-20 15:38:49 +0300

    Функция 1. Базовый визуал

--- functions/func1.js
+++ functions/func1.js
@@ -1,4 +1,14 @@
-// отчет по выставленному расписанию
+const { Markup } = require('telegraf');
+
 module.exports = async (ctx) => {
-    await ctx.reply('Функция 1 в разработке...')
-}
+    ctx.session.isInFunctionMenu = false;
+    
+    await ctx.reply('Вы выбрали: 1');
+    await ctx.reply("Пожалуйста, загрузите файл в формате XLS/XLSX", 
+        Markup.keyboard([
+            ['Вернуться в меню']
+        ])
+        .oneTime()
+        .resize()
+    );
+};
