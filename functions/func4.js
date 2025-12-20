d6924bb3eb6db63839d2d2a5c6cf2774cc271170
Ekaterina Pchelkina <ktrn.pchelkina@gmail.com>
2025-12-20 15:39:56 +0300

    Функция 4. Базовый визуал

--- functions/func4.js
+++ functions/func4.js
@@ -1,4 +1,15 @@
 // отчет по посещаемости студентов
+const { Markup } = require('telegraf');
+
 module.exports = async (ctx) => {
-    await ctx.reply('Функция 4 в разработке...')
-}
+    ctx.session.isInFunctionMenu = false;
+    
+    await ctx.reply('Вы выбрали: 4');
+    await ctx.reply("Пожалуйста, загрузите файл в формате XLS/XLSX", 
+        Markup.keyboard([
+            ['Вернуться в меню']
+        ])
+        .oneTime()
+        .resize()
+    );
+};
