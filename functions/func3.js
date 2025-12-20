93736d8a629510b45db47c1b73f66097eb69a62a
Ekaterina Pchelkina <ktrn.pchelkina@gmail.com>
2025-12-20 15:41:17 +0300

    Функция 3. Базовый визуал

--- functions/func3.js
+++ functions/func3.js
@@ -1,4 +1,15 @@
 // отчет по студентам
+const { Markup } = require('telegraf');
+
 module.exports = async (ctx) => {
-    await ctx.reply('Функция 3 в разработке...')
-}
+    ctx.session.isInFunctionMenu = false;
+    
+    await ctx.reply('Вы выбрали: 3');
+    await ctx.reply("Пожалуйста, загрузите файл в формате XLS/XLSX", 
+        Markup.keyboard([
+            ['Вернуться в меню']
+        ])
+        .oneTime()
+        .resize()
+    );
+};
