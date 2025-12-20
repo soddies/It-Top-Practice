438fbdcb6a2f0276b2917a049c399695fe5985af
Ekaterina Pchelkina <ktrn.pchelkina@gmail.com>
2025-12-20 20:10:36 +0300

    Обновление

--- functions/func4.js
+++ functions/func4.js
@@ -3,6 +3,8 @@ const { Markup } = require('telegraf');
 
 module.exports = async (ctx) => {
     ctx.session.isInFunctionMenu = false;
+    ctx.session.waitingForFile = true;
+    ctx.session.currentFunctions = 'func4';
     
     await ctx.reply('Вы выбрали: 4');
     await ctx.reply("Пожалуйста, загрузите файл в формате XLS/XLSX", 
