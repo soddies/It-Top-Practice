e024b32ae9ed37461cf857455d8f99188553eb61
Ekaterina Pchelkina <ktrn.pchelkina@gmail.com>
2025-12-30 17:17:53 +0300

    Завершена разработка 3 функции

--- functions/functionsHandlers/func3Handler.js
+++ functions/functionsHandlers/func3Handler.js
@@ -1,3 +1,54 @@
-module.exports = async (ctx, file) => {
-    await ctx.reply('Файл успешно получен! Дальнейшие действия в разработке...');
-};
+const XLSX = require('xlsx');
+
+async function func3Handlers(ctx, filePath) {
+    try {
+        const workbook = XLSX.readFile(filePath);
+        const sheet = workbook.Sheets[workbook.SheetNames[0]];
+        const rows = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+        const allStudentsBad = [];
+
+        for (let i = 1; i < rows.length; i++) {
+            const row = rows[i];
+            const fio = row[0];
+            const group = row[2];
+
+            const homework = parseFloat(row[15]);
+            const classroom = parseFloat(row[16]);
+
+            if (!isNaN(homework) && !isNaN(classroom)) {
+                if (homework == 1 && classroom > 3) {
+                    allStudentsBad.push({
+                        fio,
+                        group,
+                        classroom,
+                        homework
+                    });
+                }
+            }
+        }
+        if (allStudentsBad.length > 0) {
+            let message = "*Студенты с низкими оценками:* \n\n";
+
+            allStudentsBad.forEach((student, index) => {
+                message += `${index + 1}. ${student.fio}\n`;
+                message += `*Группа:* ${student.group}\n`;
+                message += `*Классная работа:* ${student.classroom}\n`;
+                message += `*Домашняя работа:* ${student.homework}\n\n`;
+            })
+
+            message += `*Всего студентов: ${allStudentsBad.length}*`;
+
+            await ctx.reply(message, {parse_mode: "Markdown"});
+        }
+        else {
+            await ctx.reply("Студенты с низкими оценка не найдены.");
+        }
+    }
+    catch (error) {
+        console.log('Ошибка в функции: ' + error);
+        await ctx.reply('Произошла ошибка при обработке студентов');
+    }
+}
+
+module.exports = func3Handlers;
